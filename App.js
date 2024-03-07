f32531f18215841c6a84c0c9fd6eaccbb0438596
vawzl <deyvid.matos@etec.sp.gov.br>
2024-03-07 08:44:42 -0300

    Pasta título

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Title from './Title/';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Deyvid E Tierri</Text>
+      <Text>07/03/2024</Text>
+      <Title/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ Title/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View,Text } from "react-native";
+
+export default function Title(){
+    return(
+    
+    <View>
+        <Text>
+           Aula sobre Clone e pastas. 
+        </Text>
+    </View>
+
+
+);
+}
